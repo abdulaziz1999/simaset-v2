3780e00e87ca4c04321f5640aee60b2cbae5da6b
Abdul Aziz <azizmentor96@gmail.com>
2023-09-21 15:47:23 +0700

    add modal import

---
+++ application/views/new/modal_import.php
@@ -0,0 +1,26 @@
+<div class="modal fade" id="modal-import">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Import Data</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                  <div class="form-group">
+                    <a href="<?=base_url('src/file/Import_Barang.xlsx')?>" class="btn btn-sm btn-success" ><i class="fa fa-file-excel"></i> Download Template</a>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="file">Upload File</label>
+                  <input type="file" class="form-control" id="file-mpasien" name="file">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-danger pull-left" data-dismiss="modal"> <i class="fa fa-times"></i> Close</button>
+                <button type="button" class="btn btn-sm btn-success"><i class="fa fa-upload"></i> Import</button>
+            </div>
+        </div>
+    </div>
+</div>
