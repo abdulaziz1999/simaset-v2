3377787895d21d6c7745c8a544ab8e0868465a8a
Abdul Aziz <azizmentor96@gmail.com>
2023-09-30 18:50:03 +0700

    update view print qrcode dan berita acara

---
+++ application/views/print/p_berita_acara.php
@@ -0,0 +1,178 @@
+<link rel="stylesheet" href="<?=base_url()?>src/backend/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+<link rel="stylesheet" href="<?=base_url()?>src/backend/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?=base_url()?>src/backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?=base_url()?>src/backend/dist/css/adminlte.min.css">
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="<?=base_url()?>src/backend/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+    <!-- Main content -->
+<body style="overflow-x: hidden;">
+<meta http-equiv="REFRESH" content="5; url=<?=base_url('print_berita')?>">
+    <section class="content" id="content-to-print">
+        <!-- Default box -->
+        <div class="card">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-right mr-4">
+                        <div><b><?=$detail->no_arsip?></b></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <h4 class="text-center mb-5">
+                    <strong class="wrap-text">
+                        <?php //$judul= 'PEMERINTAH DAERAH TANGERANG SELATAN DINAS LINGKUNGAN HIDUP BERITA ACARA SERAH TERIMA PEMBELIAN LAINNYA';?>
+                        <?php //echo wordwrap($judul,45,"<br>")?>
+                        <?= $judul->judul_surat?><br>
+                        <?= $judul->text_2?> <br>
+                        <?= $judul->text_3?>
+                    </strong>
+                </h4>
+                <div class="row pb-4">
+                    <div class="col-md-6">
+                        <table>
+                            <tr>
+                                <td>No Berita</td>
+                                <td>:</td>
+                                <td>&nbsp;<?= $detail->no_berita?></td>
+                            </tr>
+                            <tr>
+                                <td>Tanggal Penerimaan</td>
+                                <td>:</td>
+                                <td>&nbsp;<?= tgl_indo($detail->tgl_penerimaan)?></td>
+                            </tr>
+                            <tr>
+                                <td>Diterima Dari</td>
+                                <td>:</td>
+                                <td>&nbsp;<?= $detail->diterima_dari?></td>
+                            </tr>
+                            <tr>
+                                <td>No PPTK</td>
+                                <td>:</td>
+                                <td>&nbsp;<?= $detail->no_pptk?></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="col-md-6">
+                        <table style="float: right;">
+                            <tr>
+                                <td>Program</td>
+                                <td>:</td>
+                                <td>&nbsp;<?= $detail->program?></td>
+                            </tr>
+                            <tr>
+                                <td>Kegiatan</td>
+                                <td>:</td>
+                                <td>&nbsp;<?= $detail->kegiatan?></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table id="example1" class="table table-bordered table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Kode Barang</th>
+                                <th>Nama Barang</th>
+                                <th>Nama Umum</th>
+                                <th>Spesifikasi</th>
+                                <th>Jumlah</th>
+                                <th>Harga Barang</th>
+                                <th>Keterangan</th>
+                                <!-- <th>QR</th> -->
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $no=1; foreach ($data->result() as $row): ?>
+                            <tr>
+                                <td><?= $no++?></td>
+                                <td><?= $row->kode_barang?></td>
+                                <td><?= $row->nama_barang?></td>
+                                <td><?= $row->nama_umum?></td>
+                                <td><?= $row->spesifikasi?></td>
+                                <td><?= $row->jumlah?></td>
+                                <td><?= format_currency($row->harga)?></td>
+                                <td><?= $row->ket?></td>
+                                <!-- <td>
+                                    <?php if($row->qrcode){?>
+                                    <img src="<?= base_url('src/img/qrcode/'.$row->qrcode)?>"  class="img-thumbnail rounded" width="200" height="200">
+                                    <?php }else{?>
+                                    -
+                                    <?php }?>
+                                </td> -->
+                            </tr>
+                            <?php endforeach ?>
+                        </tbody>
+                    </table>
+
+                    <table width="100%">
+                        <tr>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr class="">
+                            <td  bgcolor="white" width="50%" align="center">&nbsp;</td>
+                            <td  bgcolor="white" width="50%" align="center">Mengetahui</td>
+                        </tr>
+                        <tr class="">
+                            <td bgcolor="white">&nbsp;</td>
+                            <td bgcolor="white">&nbsp;</td>
+                        </tr>
+                        <tr class="">
+                            <td bgcolor="white">&nbsp;</td>
+                            <td bgcolor="white">&nbsp;</td>
+                        </tr>
+                        <tr class="">
+                            <td bgcolor="white">&nbsp;</td>
+                            <td bgcolor="white">&nbsp;</td>
+                        </tr>
+                        <tr class="">
+                            <td bgcolor="white">&nbsp;</td>
+                            <td bgcolor="white">&nbsp;</td>
+                        </tr>
+                        <tr class="">
+                            <td bgcolor="white" align="center">&nbsp;</td>
+                            <td  bgcolor="white" align="center">&nbsp;(&nbsp;...........................................&nbsp;)&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+                <h4><b>Lampiran :</b></h4>
+                <div class="row">
+                    <?php foreach($file->result() as $row):?>
+                    <div class="col-md-4 mt-3">
+                        <img src="<?= base_url('src/file/'.$row->file_name)?>" class="img-thumbnail rounded" alt="...">
+                    </div>
+                    <?php endforeach;?>
+                </div>
+            </div>
+            <!-- /.card-footer-->
+        </div>
+        <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+
+<script>
+
+// $(document).ready(function () {
+    window.onload = function () {
+    window.print();
+}
+// });
+
+</script>
+
+</body>
---
+++ application/views/print/v_qrcode.php
@@ -0,0 +1,119 @@
+<link rel="stylesheet" href="<?=base_url()?>src/backend/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+<link rel="stylesheet" href="<?=base_url()?>src/backend/dropify/dropify.min.css">
+
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Print QR Code</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="<?=base_url('dashboard')?>">Dashboard</a></li>
+            <li class="breadcrumb-item active">Print QR Code</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <div class="flash-data" data-flashdata="<?=$this->session->flashdata('sukses');?>"></div>
+
+  <!-- Main content -->
+  <section class="content">
+
+    <!-- Default box -->
+    <div class="card card-primary card-outline">
+      <div class="card-header">
+        <!-- <marquee behavior="" direction="">Coming Soon !!</marquee> -->
+        <h3 class="card-title">
+        </h3>
+
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+            <i class="fas fa-minus"></i></button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fas fa-times"></i></button>
+            </div>
+          </div>
+          <div class="card-body"> 
+          <div class="table-responsive">
+          <table id="example1" class="table table-bordered table-striped table-sm">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>No Berita Acara</th>
+                <th>Tanggal Penerimaan</th>
+                <th>Diterima Dari</th>
+                <th>No Kontrak</th>
+                <th>No PPTK</th>
+                <th>Program</th>
+                <th>Kegitan</th>
+                <th>No Arsip</th>
+                <th width="20%">Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php $no=1; foreach ($data->result() as $row): ?>               
+                <tr>
+                    <td><?= $no++?></td>
+                    <td><?= $row->no_berita?></td>
+                    <td><?= tgl_indo($row->tgl_penerimaan)?></td>
+                    <td><?= $row->diterima_dari?></td>
+                    <td><?= $row->no_kontrak?></td>
+                    <td><?= $row->no_pptk?></td>
+                    <td><?= $row->program?></td>
+                    <td><?= $row->kegiatan?></td>
+                    <td><?= $row->no_arsip?></td>
+                    <td>
+                      <div class="btn-group">
+                        <button class="btn btn-sm btn-secondary"> 
+                          <i class="fa fa-briefcase"></i> &nbsp; 
+                          <span class="badge badge-light"><?= count_barang($row->id_berita)?></span>
+                        </button>
+                        &nbsp;
+                        <a href="<?= base_url('qr_code/pQrcode/'.$row->id_berita)?>" class="btn btn-sm btn-danger"> <i class="fa fa-print"></i></a>
+                      </div>
+                    </td>
+                </tr>
+              <?php endforeach ?>
+            </tbody>
+          </table>
+          </div>           
+        </div>
+        <div class="card-footer">
+
+        </div>
+      </div>
+
+    </section>
+  </div>
+
+
+  <script src="<?=base_url()?>src/backend/plugins/datatables/jquery.dataTables.js"></script>
+  <script src="<?=base_url()?>src/backend/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+  <script src="<?=base_url()?>src/backend/dropify/dropify.min.js"></script>
+  <script>
+    $(function () {
+      $("#example1").DataTable({
+        "language": {
+          "sSearch": "Cari"
+        }
+      });
+
+      $('.dropify').dropify();
+        // messages: {
+        //           default: 'Drag atau drop untuk memilih gambar',
+        //           replace: 'Ganti', 
+        //           remove:  'Hapus',
+        //           error:   'error'
+        //       }
+      // });
+    });
+    function tombolUpload(id){
+        $('#berita_id').val(id);
+      }
+
+  </script>
